4bf70ac9e934d8ac92096682d85744b0d10520c7
eenees <soeteniels1803@gmail.com>
2024-11-20 13:42:20 +0100

    remove todo

--- src/state/state.js
+++ src/state/state.js
@@ -6,6 +6,7 @@ import * as Widget from "../objects/widget/widget.js";
 export function createState() {
   const state = {
     widget: Widget.createWidget(),
+    // TODO: add history to keep track what has been changed.
   };
   return state;
 };
