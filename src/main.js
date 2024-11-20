211853c83a869d8d37aac12fa8c838b9124c1358
eenees <soeteniels1803@gmail.com>
2024-11-20 17:06:47 +0100

    styling changes

--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@ import * as State from "./state/state.js";
 import * as UI from "./ui/ui.js";
 import * as Selector from "./objects/selector/selector.js";
 
-const $openBtn = document.querySelector(".open-widget-btn");
-const $closeBtn = document.querySelector(".close-widget-btn");
+const $toggleWidgetBtn = document.querySelector(".open-widget-btn");
 const $toggleSelectorBtn = document.querySelector(".toggle-selector-btn");
 
 function init() {
@@ -18,15 +17,11 @@ function init() {
  * @param {State} state 
  */
 function addListeners(state) {
-  $openBtn.addEventListener('click', function() {
+  $toggleWidgetBtn.addEventListener('click', function() {
     Widget.toggle(state);
-    Bus.publish('open-widget', {});
+    Bus.publish('toggle-widget', {});
   });
 
-  $closeBtn.addEventListener('click', function() {
-    Widget.toggle(state);
-    Bus.publish('close-widget', {});
-  });
   $toggleSelectorBtn.addEventListener('click', function() {
     Selector.toggle(state);
     Bus.publish('toggle-selector', { isActive: state.widget.selector.isActive });
--- src/ui/ui.js
+++ src/ui/ui.js
@@ -2,16 +2,13 @@ import * as Bus from "../bus.js";
 import * as Utils from "../utils.js";
 
 export function init() {
-  Bus.listen('open-widget', toggleWidget);
-  Bus.listen('close-widget', toggleWidget);
+  Bus.listen('toggle-widget', toggleWidget);
   Bus.listen('toggle-selector', toggleSelector);
   Bus.listen('element-selected', handleElementSelected);
 };
 
 function toggleWidget() {
-  const $openBtn = document.querySelector(".open-widget-btn");
   const $openWidgetContainer = document.querySelector(".open-widget-container");
-  $openBtn.classList.toggle("hidden");
   $openWidgetContainer.classList.toggle("hidden");
 }
 
@@ -20,11 +17,16 @@ function toggleWidget() {
  */
 function toggleSelector(e) {
   const { isActive } = e.detail;
+
+  /** @type {HTMLElement|null} */
   const $toggleSelectorButton = document.querySelector(".toggle-selector-btn");
+
   if (isActive) {
     hideEditors();
-  }
-  $toggleSelectorButton.textContent = isActive ? "Disable Selector" : "Start Selector";
+    $toggleSelectorButton.style.fill = "#0084FF";
+  } else {
+    $toggleSelectorButton.style.fill = "black";
+  };
 }
 
 /**
@@ -36,8 +38,6 @@ function handleElementSelected(e) {
     loadTextEditor();
   };
 };
-
-// TODO: when resetting hide all the editors
 
 function loadTextEditor() {
   const $textElementEditor = document.querySelector(".text-element-editor");
