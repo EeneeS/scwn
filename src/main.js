98782d00f1dac251cc7bec33c37b4896f77660e3
eenees <soeteniels1803@gmail.com>
2024-11-26 18:47:41 +0100

    clean up

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import * as Widget from "./objects/widget/widget.js";
 import * as State from "./state/state.js";
 import * as UI from "./ui/ui.js";
 import * as Selector from "./objects/selector/selector.js";
-import * as Editor from "./objects/editor/editor.js";
 import * as EditorSave from "./objects/editor/save.js";
 
 const $toggleWidgetBtn = document.querySelector(".open-widget-btn");
@@ -12,8 +11,6 @@ const $saveChangesBtn = document.querySelector(".save-editor-changes");
 const $cancelChangesBtn = document.querySelector(".cancel-editor-changes");
 const $undoChangeBtn = document.querySelector(".undo-editor-change");
 const $redoChangeBtn = document.querySelector(".redo-editor-change");
-const $textSizeDecrbtn = document.querySelector(".text-size-decr-btn");
-const $textSizeIncrbtn = document.querySelector(".text-size-incr-btn");
 //const $moveLeftBtn = document.querySelector(".widget-move-left");
 //const $moveRightBtn = document.querySelector(".widget-move-right");
 
@@ -73,6 +70,6 @@ function addListeners(state) {
 export const version = "1.0.0";
 export { init };
 
-//init({
-//  position: "right",
-//}); // for development
+init({
+  position: "right",
+}); // for development
