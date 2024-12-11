b2419765b6617622796a6dd5c00cf5053263fbc7
eenees <soeteniels1803@gmail.com>
2024-12-11 10:37:33 +0100

    change hisotory to change log

--- src/objects/history/history.js
+++ src/objects/history/history.js
@@ -4,42 +4,43 @@ import { api } from "../../api.js";
  * @param {State} state 
  */
 export async function loadHistory(state) {
-  const projectId = state.options.projectId;
-  const result = await api(`/projects/${projectId}/changes`, "GET");
-  renderHistory(result);
+  const changes = state.widget.editor.changes;
+  populateHistoryContainer(changes);
 };
 
 /**
- * @param {Array} data
+ * @param {Change[]} data
  */
-function renderHistory(data) {
+function populateHistoryContainer(data) {
   const $container = document.querySelector(".history-items");
-
-  const groupedArray = groupData(data);
-
-  // TODO: style this better :)
-  let html = '';
-  groupedArray.forEach(([batch_id, items], index) => {
-    const className = index % 2 === 0 ? 'history-item-light' : 'history-item-dark';
-    html += `<div class="${className} batch" id="${batch_id}">`;
-    items.forEach((/** @type {{ element: string; type: string; original_value: string; new_value: string; route: string; }} */ item) => {
-      html += `<p>${item.element} - ${item.type} - from ${item.original_value} to ${item.new_value} - ${item.route}</p>`;
-    });
-    html += `</div>`;
+  let html = "";
+  data.forEach((change, index) => {
+    const className = index % 2 == 0 ? "history-item-light" : "history-item-dark";
+    html += generateChangeHTML(change, className);
   });
-
-  $container.innerHTML = html;
+  $container.insertAdjacentHTML("beforeend", html);
 };
 
 /**
- * @param {Array} data 
+ * @param {Change} change 
+ * @param {string} className 
   */
-function groupData(data) {
-  const grouped = data.reduce((acc, item) => {
-    const { batch_id } = item;
-    if (!acc[batch_id]) acc[batch_id] = [];
-    acc[batch_id].push(item);
-    return acc;
-  }, {});
-  return Object.entries(grouped);
+function generateChangeHTML(change, className) {
+  return `<div class="${className}">${generateHTMLForChangeType(change)}</div>`;
+}
+
+/**
+ * @param {Change} change 
+  */
+function generateHTMLForChangeType(change) {
+  const type = change.type
+  if (type === "text-value") {
+    return `Changed <span>${change.original}</span> to <span>${change.newValue}</span>`;
+  } else if (type === "text-color") {
+    return `Changed to color of <span>${change.el.innerHTML}</span> to <span style="color: ${change.newValue}">${change.newValue.toUpperCase()}</span>`;
+  } else if (type === "text-size") {
+    return `Changed font size of <span>${change.el.innerHTML}</span> to ${change.newValue}`;
+  } else {
+    return `Added ${change.newValue} to <span>${change.el.innerHTML}</span>`;
+  };
 }
